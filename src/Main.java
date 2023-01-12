24e587a80d54fa000a53c1db3d236eee7830152f
Mwai <machariamwai@gmail.com>
2023-01-12 10:04:19 +0300

    OOP3 Done

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 //        Engine engine = new Engine ("Renault", 8000);
-        Car mercedes = new Car("Mercedes AMG", 2, "Silver", new Engine("Renault", 8000));
+//        Car mercedes = new Car("Mercedes AMG", 2, "Silver", new Engine("Renault", 8000));
+//
+//        System.out.println(mercedes.getName());
+//        Engine engine = mercedes.getEngine();
+//        System.out.println("Engine Model: " + engine.getModel());
 
-        System.out.println(mercedes.getName());
-        Engine engine = mercedes.getEngine();
-        System.out.println("Engine Model: " + engine.getModel());
+
+//        Car mercedes = null;
+//        if (mercedes != null) {
+//            mercedes.getName();
+//        } else {
+//            System.out.println("The car was null!");
+//        }
+
+//        final int a = 5;
+//        a = a + 1;
+
+        final Engine engine = new Engine("Renault", 8000);
+//        engine = new Engine("Renault", 7000);
+
+        engine.setNpm(10000);
     }
 }
